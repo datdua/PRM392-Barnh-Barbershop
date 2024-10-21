5ac3752584f6d9adf33b942d806e75c40cdf81f6
VuonQ <vuongtg0@gmail.com>
2024-10-21 13:49:20 +0700

    fix fragment

--- app/src/main/java/com/example/project_hairbarbershop_g5/ui/account/AccountViewModel.java
+++ app/src/main/java/com/example/project_hairbarbershop_g5/ui/account/AccountViewModel.java
@@ -1,14 +1,14 @@
-package com.example.project_hairbarbershop_g5.ui.notifications;
+package com.example.project_hairbarbershop_g5.ui.account;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class AccountViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public AccountViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is notifications fragment");
     }
--- app/src/main/java/com/example/project_hairbarbershop_g5/ui/history/HistoryViewModel.java
+++ app/src/main/java/com/example/project_hairbarbershop_g5/ui/history/HistoryViewModel.java
@@ -1,14 +1,14 @@
-package com.example.project_hairbarbershop_g5.ui.dashboard;
+package com.example.project_hairbarbershop_g5.ui.history;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class HistoryViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public HistoryViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
